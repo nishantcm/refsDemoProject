57cd86736001dda6d1eba841f3efe692c29d0981
cmnishant <cmnishantwork@gmail.com>
2025-02-10 00:09:01 +0530

    Updated the file with NoProjectSelected file import

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSiderbar from "./components/ProjectsSidebar.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSiderbar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
