d387b41a8f3eabcc563be3f1b503e7bdecf65db1
cmnishant <cmnishantwork@gmail.com>
2025-02-13 00:13:30 +0530

    Updated the file with Modal

--- 01-starting-project/src/components/NewProject.jsx
+++ 01-starting-project/src/components/NewProject.jsx
@@ -1,7 +1,11 @@
 import { useRef } from 'react';
+
 import Input from './Input.jsx';
+import Modal from './Modal.jsx';
 
 export default function NewProject({onAdd}) {
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -11,17 +15,29 @@ export default function NewProject({onAdd}) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
-        // validation ...
+        if (
+            enteredTitle.trim() === '' || 
+            enteredDescription.trim() === '' || 
+            enteredDueDate.trim() === '' 
+        ) {
+            modal.current.open();
+            return; 
+        }
 
         onAdd ({
             title: enteredTitle,
             description: enteredDescription,
             dueDate: enteredDueDate
         });
-
     }
 
     return (
+        <>
+        <Modal ref={modal} buttonCaption="Okay">
+            <h2>Invalid Input</h2>
+            <p>Oops ... looks like you forgot to enter a value.</p>
+            <p>Please make sure you provide a valid value for every input field.</p>
+        </Modal>
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li>
@@ -42,5 +58,6 @@ export default function NewProject({onAdd}) {
                 <Input type="date" ref={dueDate} label="Due Date" />
             </div>
         </div>
+        </>
     );
 }
