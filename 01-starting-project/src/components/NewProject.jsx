7542c5bcc3e890372e773c07df190eb033be1535
cmnishant <cmnishantwork@gmail.com>
2025-02-15 02:03:30 +0530

    Updated the NewProject.jsx file

--- 01-starting-project/src/components/NewProject.jsx
+++ 01-starting-project/src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import Input from './Input.jsx';
 import Modal from './Modal.jsx';
 
-export default function NewProject({onAdd}) {
+export default function NewProject({onAdd, onCancel}) {
     const modal = useRef();
 
     const title = useRef();
@@ -34,19 +34,24 @@ export default function NewProject({onAdd}) {
     return (
         <>
         <Modal ref={modal} buttonCaption="Okay">
-            <h2>Invalid Input</h2>
-            <p>Oops ... looks like you forgot to enter a value.</p>
-            <p>Please make sure you provide a valid value for every input field.</p>
+            <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+            <p className="text-stone-600 mb-4">Oops ... looks like you forgot to enter a value.</p>
+            <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
         </Modal>
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li>
-                    <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                    <button 
+                        className="text-stone-800 hover:text-stone-950"
+                        onClick={onCancel}
+                    >
+                        Cancel
+                    </button>
                 </li>
                 <li>
                     <button 
-                    className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-                    onClick={handleSave}>
+                        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                        onClick={handleSave}>
                         Save
                     </button>
                     
