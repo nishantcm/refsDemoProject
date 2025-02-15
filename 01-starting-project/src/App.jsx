bd48c95af805c228c30db9dfb4cb822ad3e6342e
cmnishant <cmnishantwork@gmail.com>
2025-02-16 02:00:14 +0530

    Updated the App.jsx file

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSiderbar from "./components/ProjectsSidebar.jsx";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
   });
 
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    }) ;
+  }
+
   function handleStartAddProject() {
-    setProjectsState(prevState => {
+    setProjectsState((prevState) => {
       return {
         ...prevState,
         selectedProjectId: null,
@@ -19,7 +29,7 @@ function App() {
   }
 
   function handleCancelAddProject() {
-    setProjectsState(prevState => {
+    setProjectsState((prevState) => {
       return {
         ...prevState,
         selectedProjectId: undefined,
@@ -43,7 +53,9 @@ function App() {
     });
   }
 
-  let content;
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
@@ -56,6 +68,7 @@ function App() {
       <ProjectsSiderbar 
         onStartAddProject={handleStartAddProject} 
         projects={projectsState.projects} 
+        onSelectProject={handleSelectProject}
       />
       {content}
     </main>
